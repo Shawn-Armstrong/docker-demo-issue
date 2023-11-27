52ae12a1dfca5aff33da71dd215e91c2a72943d3
Shawn Armstrong <smarmstr@ucsc.edu>
2023-11-26 22:24:27 -0800

    Last commit

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark>
+              <v-toolbar-title>Register</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-text-field
+                label="Username"
+                prepend-icon="mdi-account"
+                type="text"
+                v-model="username"
+              ></v-text-field>
+              <v-text-field
+                label="Email"
+                prepend-icon="mdi-email"
+                type="email"
+                v-model="email"
+              ></v-text-field>
+              <v-text-field
+                label="Password"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="password"
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="secondary" @click="submit">Register</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import axios from 'axios';
+  
+  const username = ref('');
+  const email = ref('');
+  const password = ref('');
+  
+  const submit = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/register', {
+        username: username.value,
+        email: email.value,
+        password: password.value
+      });
+      console.log('Registration successful:', response.data);
+      
+      // You might want to log in the user directly,
+      // save the token, and redirect to a dashboard or home page.
+  
+      // Redirect the user to the login page or a different page
+      // this.$router.replace({ path: 'login' });
+    } catch (error) {
+      console.error('Registration failed:', error.response.data);
+    }
+  };
+  </script>
+  
+  <style>
+  /* Add any additional styling you want for your registration component here */
+  </style>
+  
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,43 @@
 <template>
-  <Login />
+  <v-container>
+    <v-row justify="center">
+      <v-btn class="mx-12" color="primary" dark v-if="!showLogin && !showRegister" @click="showLoginComponent">Login</v-btn>
+      <v-btn class="mx-12" color="primary" dark v-if="!showLogin && !showRegister" @click="showRegisterComponent">Register</v-btn>
+    </v-row>
+
+    <v-row v-if="showLogin || showRegister">
+      <v-btn class="mx-12" color="primary" dark @click="backToSelection">Back</v-btn>
+    </v-row>
+
+    <Login v-if="showLogin" />
+    <Register v-if="showRegister" />
+  </v-container>
 </template>
 
 <script setup>
-  import Login from '@/components/Login.vue';
+import { ref } from 'vue';
+import Login from '@/components/Login.vue';
+import Register from '@/components/Register.vue';
+
+const showLogin = ref(false);
+const showRegister = ref(false);
+
+const showLoginComponent = () => {
+  showLogin.value = true;
+  showRegister.value = false;
+};
+
+const showRegisterComponent = () => {
+  showLogin.value = false;
+  showRegister.value = true;
+};
+
+const backToSelection = () => {
+  showLogin.value = false;
+  showRegister.value = false;
+};
 </script>
+
+<style>
+/* Add any additional styling you want here */
+</style>
